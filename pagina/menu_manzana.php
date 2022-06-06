994bda9ed7d1ec184037e894f887924609902634
Juan Pablo Zapata <juan.zapata103@udea.edu.co>
2022-06-06 17:54:53 -0500

    Inicio de sesión
    Login correcto

--- pagina/menu_manzana.php
+++ pagina/menu_manzana.php
@@ -8,6 +8,21 @@
     <LINK REL=StyleSheet HREF="../CSS/estilo_login.css">
 </head>
 <body>
+<?php
+    //Incluir el archivo que contiene las funciones del lenguaje PHP
+    require_once("../PHP/funciones.php");
+    //Desactivar Desactivar toda notificación de error
+    error_reporting(0);
+    $usuario     =      $_POST['u'];
+    $clave       =      $_POST['p'];
+
+    $tipo_de_cuenta = iniciar_sesion($usuario, $clave);
+
+    echo " - ".$tipo_de_cuenta;
+    // Notificar todos los errores de PHP
+    error_reporting(-1);
+?>
+
 <div class="menu">
 
 </div>
---
+++ php/funciones.php
@@ -0,0 +1,61 @@
+<?php
+
+    //Funcion que verifica si existeel archivo de conexion de la base de datos
+    function existencia_de_la_conexion(){
+        try {
+            //Verificar si existe el archivo de conexion
+            if(!file_exists('../PHP/conexion.php')){
+                throw new Exception ('PHP: File  -conexion-  no existe',1);  //NO existe, captura excepcion
+            }else{
+                return true;
+                //require_once("../PHP/conexion.php");                //SI Existe, continuar y realizar la conexion
+            }
+        
+        } catch (Exception $excepcion) {
+            //Captura de excepcion y su respectivo codigo
+            echo 'Capture: ' .  $excepcion->getMessage(), "<br>";
+            echo 'Código: ' . $excepcion->getCode(), "<br>";
+        }
+    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function iniciar_sesion($usuario, $clave){
+        
+        if(existencia_de_la_conexion()){
+            require_once("../PHP/conexion.php");    //Hacer conexion con la base de datos
+        }
+        $conexion = conectar();                     //Obtenemos la conexion
+        
+        //Consulta a la base de datos en la tabla login
+        $consulta = mysqli_query($conexion, "SELECT `user_pers`, `pass_pers`, `tipo_usuario_pers` FROM `personal` WHERE `estado` ='activo'")
+        or die ("Error al iniciar sesión: ");
+
+        $encontrado = false;
+        while (($fila = mysqli_fetch_array($consulta))!=NULL){
+
+        //Comprobamos la existencia del usuario y contraseña del formulario en los resulatdos de la bases de datos
+            if($usuario == $fila['user_pers'] && $clave == $fila['pass_pers']){
+                //Existe en la base de datos y es conrrecto los datos
+                $tipo_de_cuenta = $fila['tipo_usuario_pers']; //Obtenemos su tipo de cuenta
+                echo $fila['user_pers'];
+                $encontrado = true;
+                mysqli_free_result($consulta); //Liberar espacio de consulta cuando ya no es necesario
+                mysqli_close($conexion);     //---------------------- Cerrar conexion ------------------
+                break;
+            }
+        }
+        if($encontrado==false){
+            mysqli_free_result($consulta); //Liberar espacio de consulta cuando ya no es necesario
+            mysqli_close($conexion);     //---------------------- Cerrar conexion ------------------
+            //Si no se encontró registro alguno, regresamos al index de inicio de sesión
+            ?>
+            <script type="text/javascript">
+				window.history.back();
+			</script>
+            <?php
+        }
+
+        return $tipo_de_cuenta;
+        
+    }
+?>
